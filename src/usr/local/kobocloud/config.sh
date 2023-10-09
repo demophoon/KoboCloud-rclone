c024a4332ec68280b7daf4de867a1cce4ce8af58
Britt Gresham <britt@brittg.com>
2023-10-09 14:22:20 -0700

    Remove more unused files

--- src/usr/local/kobocloud/config.sh
+++ src/usr/local/kobocloud/config.sh
@@ -3,11 +3,26 @@
 KC_HOME=$(dirname $0)
 ConfigFile=$KC_HOME/kobocloudrc.tmpl
 
-case ${DEBUG:-0} in
-    1)
-        . $KC_HOME/config_pc.sh
-        ;;
-    *)
-        . $KC_HOME/config_kobo.sh
-        ;;
-esac
+# Kobo Configs
+Logs=/mnt/onboard/.add/kobocloud
+Lib=/mnt/onboard/.add/kobocloud/Library
+SD=/mnt/sd/kobocloud
+UserConfig=/mnt/onboard/.add/kobocloud/kobocloudrc
+RCloneConfig=/mnt/onboard/.add/kobocloud/rclone.conf
+Dt="date +%Y-%m-%d_%H:%M:%S"
+RCLONEDIR="/mnt/onboard/.add/kobocloud/bin/"
+RCLONE="${RCLONEDIR}rclone"
+PLATFORM=Kobo
+
+# PC Configs
+if [ ${DEBUG:-0} = 1 ]; then
+    Logs=/tmp/KoboCloud
+    Lib=/tmp/KoboCloud/Library
+    SD=/tmp/KoboCloud/sd
+    UserConfig=./kobocloudrc.tmpl
+    RCloneConfig=~/.config/rclone/rclone.conf
+    Dt="date +%Y-%m-%d_%H:%M:%S"
+    RCLONEDIR="/usr/bin/"
+    RCLONE="$(which rclone)"
+    PLATFORM=PC
+fi
